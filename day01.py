c58a12bac4059f57572d020bcad0d33e648053ce
Krzysztof Zawadka <krzysztof.zawadka@pl.markant.com>
2022-12-05 12:04:18 +0100

    Day 1 solution

--- day01.py
+++ day01.py
@@ -1,7 +1,7 @@
-data = ""
-with open("data/test-day01.txt", "r") as file:
-    data = file.read()
+with open("data/day01.txt", "r") as file:
+    data = [[int(cal) for cal in elf.split("\n")] for elf in file.read().split("\n\n")]
 
-inputstream = [[int(j) for j in ] elf.split("\n") for elf in data.split("\n\n")]
+elvesWithCalories = sorted([sum(x) for x in data], reverse=True)
 
-print("bad")
+print("Part 1:", elvesWithCalories[0])
+print("Part 2:", elvesWithCalories[0] + elvesWithCalories[1] + elvesWithCalories[2])
