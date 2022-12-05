a7f03e13b1c5e402b11a77a5b8dfdddbda716fe4
Krzysztof Zawadka <krzysztof.zawadka@pl.markant.com>
2022-12-05 12:05:43 +0100

    Changed gitignore file

--- day01.py
+++ day01.py
@@ -1,7 +1,14 @@
 with open("data/day01.txt", "r") as file:
-    data = [[int(cal) for cal in elf.split("\n")] for elf in file.read().split("\n\n")]
+    data = sorted(
+        [
+            sum(x)
+            for x in [
+                [int(cal) for cal in elf.split("\n")]
+                for elf in file.read().split("\n\n")
+            ]
+        ],
+        reverse=True,
+    )
 
-elvesWithCalories = sorted([sum(x) for x in data], reverse=True)
-
-print("Part 1:", elvesWithCalories[0])
-print("Part 2:", elvesWithCalories[0] + elvesWithCalories[1] + elvesWithCalories[2])
+print("Part 1:", data[0])
+print("Part 2:", data[0] + data[1] + data[2])
