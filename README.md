659f2910ec7f8bf92c77c26dc9f817a915186e6a
Krzysztof Zawadka <kzawadka@gmail.com>
2022-12-08 22:18:57 +0100

    Revert "Small refactor"
    This reverts commit 8d2bec24589a3ffd195a978d2acff04b511b5453.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Advent Of Code 2022
-Solutions of [Advent of Code 2022](https://adventofcode.com/2022) written in Python.
+Advent of Code 2022
--- day01.py
+++ day01.py
@@ -1,13 +1,14 @@
-data = sorted(
-    [
-        sum(x)
-        for x in [
-            [int(cal) for cal in elf.split("\n")]
-            for elf in open("data/day01.txt", "r").read().split("\n\n")
-        ]
-    ],
-    reverse=True,
-)
+with open("data/day01.txt", "r") as file:
+    data = sorted(
+        [
+            sum(x)
+            for x in [
+                [int(cal) for cal in elf.split("\n")]
+                for elf in file.read().split("\n\n")
+            ]
+        ],
+        reverse=True,
+    )
 
 print("Part 1:", data[0])
 print("Part 2:", data[0] + data[1] + data[2])
