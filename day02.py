8d2bec24589a3ffd195a978d2acff04b511b5453
Krzysztof Zawadka <kzawadka@gmail.com>
2022-12-08 22:16:49 +0100

    Small refactor

--- day02.py
+++ day02.py
@@ -1,11 +1,8 @@
 outcome = ["BX", "CY", "AZ", "AX", "BY", "CZ", "CX", "AY", "BZ"]
 outcome2 = ["BX", "CX", "AX", "AY", "BY", "CY", "CZ", "AZ", "BZ"]
 
-with open("data/day02.txt", "r") as file:
-    data = [x.strip().replace(" ", "") for x in file.readlines()]
-
-score = 0
-score2 = 0
+data = [x.strip().replace(" ", "") for x in open("data/day02.txt", "r").readlines()]
+score, score2 = 0, 0
 for duel in data:
     score += outcome.index(duel) + 1
     score2 += outcome2.index(duel) + 1
