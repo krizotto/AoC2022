18064b0bc3acd8f92a42582ded319f9b3c30b3ce
Krzysztof Zawadka <krzysztof.zawadka@pl.markant.com>
2022-12-09 06:27:37 +0100

    Day 4 refactor

--- day04.py
+++ day04.py
@@ -1,17 +1,25 @@
-data = [
-    [x.split("-"), y.split("-")]
-    for x, y in [z.split(",") for z in [x.strip() for x in open("data/day04.txt", "r")]]
-]
+import re
+
+
+def solution(a, b, c, d):
+    global count_includes
+    global count_intersections
+    a_set = set(range(a, b + 1))
+    b_set = set(range(c, d + 1))
+    if a_set.issubset(b_set) or b_set.issubset(a_set):
+        count_includes += 1
+    if a_set & b_set:
+        count_intersections += 1
+
+
 count_intersections = 0
 count_includes = 0
-for pair in data:
-    pair = [[int(a), int(b)] for a, b in pair]
-    range_a = set(range(pair[0][0], pair[0][1] + 1))
-    range_b = set(range(pair[1][0], pair[1][1] + 1))
-    if range_a.issubset(range_b) or range_b.issubset(range_a):
-        count_includes += 1
-    if range_a & range_b:
-        count_intersections += 1
+[
+    solution(int(a), int(b), int(c), int(d))
+    for a, b, c, d in [
+        re.split("[-,]", z) for z in [x.strip() for x in open("data/day04.txt", "r")]
+    ]
+]
 
 print("Part 1:", count_includes)
 print("Part 2:", count_intersections)
